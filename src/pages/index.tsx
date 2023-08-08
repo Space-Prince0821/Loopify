98602ef97986e74e61401f132a372237cd69dab8
Prince <Prince.chowdhury.246@my.csun.edu>
2023-08-07 23:25:53 -0400

    Homepage for quizify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,13 +9,19 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Loopify</title>
-        <meta name="description" content="Loopify, best new gen social media" />
+        <title>Quizify</title>
+        <meta name="description" content="Quizify, best next gen quiz website to study for exams" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-slate-900 text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <SignInButton />
+          {/* <SignInButton /> */}
+
+          <h1 className="text-3xl">Quizify</h1>
+
+          <Link href='/quiz'>
+            <button className="border-2 border-slate-400 px-4 py-2 rounded-full hover:bg-slate-700">Start Quiz!</button>
+          </Link>
         </div>
       </main>
     </>
