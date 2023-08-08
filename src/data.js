3ebb2511329f5c38537e23641f26bf97e9b62df0
Prince <Prince.chowdhury.246@my.csun.edu>
2023-08-08 00:07:01 -0400

    Adding js files

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const quiz = {
+    totalQuestions: 3,
+    questions: [
+        {
+            id: 1,
+            question: "What is the capital of France?",
+            answers: ["Madrid", "Paris", "Rome", "Berlin"],
+            correctAnswer: 'Paris',
+        },
+        {
+            id: 2,
+            question: "What is the largest planet in our solar system?",
+            answers: ["Mars", "Jupiter", "Venus", "Saturn"],
+            correctAnswer: 'Jupiter',
+        },
+        {
+            id: 3,
+            question: "What is the smallest country in the world?",
+            answers: ["Monaco", "Maldives", "Vatican City", "San Marino"],
+            correctAnswer: 'Vatican City',
+        },
+    ]
+}
