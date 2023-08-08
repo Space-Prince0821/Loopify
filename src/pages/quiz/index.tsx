ea215ce3319469556b0e6d1ae047ae770a4af3e8
Prince <Prince.chowdhury.246@my.csun.edu>
2023-08-07 23:47:26 -0400

    Adding home

---
+++ src/pages/quiz/index.tsx
@@ -0,0 +1,11 @@
+export default function Quiz() {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-slate-900 text-white">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <h1 className="text-3xl">Start Quiz</h1>
+        </div>
+      </main>
+    </>
+  );
+}
